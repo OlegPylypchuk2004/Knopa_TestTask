74b5dc7a315147405ad9c4f8a415c2a741ef1c82
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-04-03 19:42:02 +0300

    Update PlanetaryObjectFactory.cs

--- Assets/Game/Scripts/PlanetaryObjectFactory.cs
+++ Assets/Game/Scripts/PlanetaryObjectFactory.cs
@@ -9,27 +9,28 @@ public class PlanetaryObjectFactory
         _massSpecificationDatas = massSpecificationDatas;
     }
 
-    internal PlanetaryObject Create(float mass, Transform parent)
+    public PlanetaryObject Create(float mass, Transform parent)
     {
         GameObject planetGO = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         planetGO.transform.SetParent(parent);
-
-        MassSpecificationData massSpecificationData = null;
-
-        for (int i = 0; i < _massSpecificationDatas.Length; i++)
-        {
-            if (_massSpecificationDatas[i].MinMass <= mass && _massSpecificationDatas[i].MaxMass >= mass)
-            {
-                massSpecificationData = _massSpecificationDatas[i];
-
-                break;
-            }
-        }
 
         Vector3 orbitCenter = Vector3.zero;
         float orbitRadius = Random.Range(5f, 50f);
         float orbitSpeed = Random.Range(0.1f, 2f);
 
-        return new PlanetaryObject(mass, massSpecificationData, orbitCenter, orbitRadius, orbitSpeed, planetGO.transform);
+        return new PlanetaryObject(mass, DetermineMassSpecificationData(mass), orbitCenter, orbitRadius, orbitSpeed, planetGO.transform);
+    }
+
+    public MassSpecificationData DetermineMassSpecificationData(float mass)
+    {
+        for (int i = 0; i < _massSpecificationDatas.Length; i++)
+        {
+            if (_massSpecificationDatas[i].MinMass <= mass && _massSpecificationDatas[i].MaxMass >= mass)
+            {
+                return _massSpecificationDatas[i];
+            }
+        }
+
+        return null;
     }
 }
