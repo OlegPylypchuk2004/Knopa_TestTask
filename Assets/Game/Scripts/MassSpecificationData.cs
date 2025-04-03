74dbdbed2ca45f511a94bdb35da2f40637d08859
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-04-03 19:02:57 +0300

    Added configs

---
+++ Assets/Game/Scripts/MassSpecificationData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MassSpecificationData", menuName = "Data/MassSpecification")]
+public class MassSpecificationData : ScriptableObject
+{
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public float MinMass { get; private set; }
+    [field: SerializeField] public float MaxMass { get; private set; }
+    [field: SerializeField] public float MinRadius { get; private set; }
+    [field: SerializeField] public float MaxRadius { get; private set; }
+
+    private void OnValidate()
+    {
+        if (MinMass > MaxMass)
+        {
+            MaxMass = MinMass;
+        }
+
+        if (MinRadius > MaxRadius)
+        {
+            MaxRadius = MinRadius;
+        }
+    }
+}
