9db2d8324030edef74254a8ac7d2f19a9152d131
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-04-04 11:36:48 +0300

    Added pause button

--- Assets/Game/Scripts/UIManager.cs
+++ Assets/Game/Scripts/UIManager.cs
@@ -5,19 +5,27 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private PlaneterySystemManager _gameManager;
     [SerializeField] private Button _generateButton;
+    [SerializeField] private Button _pauseButton;
 
     private void OnEnable()
     {
         _generateButton.onClick.AddListener(OnGenerateButtonClicked);
+        _pauseButton.onClick.AddListener(OnPauseButtonClicked);
     }
 
     private void OnDisable()
     {
         _generateButton.onClick.RemoveListener(OnGenerateButtonClicked);
+        _pauseButton.onClick.RemoveListener(OnPauseButtonClicked);
     }
 
     private void OnGenerateButtonClicked()
     {
         _gameManager.GeneratePlaneterySystem();
+    }
+
+    private void OnPauseButtonClicked()
+    {
+        Time.timeScale = Mathf.Abs(Time.timeScale - 1f);
     }
 }
